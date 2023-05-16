dc3d8a2d41e6bbb661464f9ac47a586e0e941203
George <glomukhin@gmail.com>
2023-05-17 01:14:14 +0700

    fix disconnect, when keys not in net, + implementation mostFlights

--- src/exercises/FlightsNet.scala
+++ src/exercises/FlightsNet.scala
@@ -13,23 +13,31 @@ object FlightsNet extends App{
   def connect(network: Map[String, Set[String]], pointA: String, pointB: String): Map[String, Set[String]] = {
 
     network
-        .updated(pointA, network.getOrElse(pointA, Set.empty) + pointB)
-        .updated(pointB, network.getOrElse(pointB, Set.empty) + pointA)
+      .updated(pointA, network.getOrElse(pointA, Set.empty) + pointB)
+      .updated(pointB, network.getOrElse(pointB, Set.empty) + pointA)
   }
 
   def disconnect(network: Map[String, Set[String]], pointA: String, pointB: String): Map[String, Set[String]] = {
 
-    network
-      .updated(pointA, network.getOrElse(pointA, Set.empty) - pointB)
-      .updated(pointB, network.getOrElse(pointB, Set.empty) - pointA)
+    if (network.contains(pointA) && network.contains(pointB)) {
+      network
+        .updated(pointA, network.getOrElse(pointA, Set.empty) - pointB)
+        .updated(pointB, network.getOrElse(pointB, Set.empty) - pointA)
+    } else if (network.contains(pointA)) {
+      network
+        .updated(pointA, network.getOrElse(pointA, Set.empty) - pointB)
+    } else if (network.contains(pointB)) {
+      network
+        .updated(pointB, network.getOrElse(pointB, Set.empty) - pointA)
+    } else network
   }
 
   def nFlights(network: Map[String, Set[String]], location: String) = {
-    0
+    network.getOrElse(location, Set.empty).size
   }
 
   def mostFlights(network: Map[String, Set[String]]): String = {
-    ""
+    network.maxByOption(_._2.size).get._1
   }
 
   def nLocationsWithNoFlights(network: Map[String, Set[String]]): String = {
@@ -42,13 +50,15 @@ object FlightsNet extends App{
 
   var network : Map[String, Set[String]] = Map.empty
   network = connect(network, "A", "B")
-  network = connect(network, "A", "C")
+  network = connect(network, "B", "C")
   println(network)
 
-  network = disconnect(network, "B", "C")
+  network = disconnect(network, "B", "B")
   println(network)
 
-  network = disconnect(network, "B", "A")
+  network = disconnect(network, "D", "A")
   println(network)
+
+  println(mostFlights(network))
 
 }
